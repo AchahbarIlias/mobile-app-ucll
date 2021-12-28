49fd515d89210eebf0cf6a43daa19a48c2381148
Ilias Achahbar <ilias.achahbar@student.ucll.be>
2021-12-28 14:38:24 +0100

    first init

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const bodyParser= require('body-parser')
+const app = express();
+
+app.use(bodyParser.json())
+let port = 3000
+
+app.listen(port, function() {
+    console.log("Server running on port: " + port);
+})
+
+
+var users = [{ username: "ilias", latitude: "13.0000", longitude: "20.0000" }, { username: "ilias2", latitude: "10.0000", longitude: "10.0000"}];
+
+app.get("/users", function(req, res) {
+    console.log(users);
+    res.send(users);
+})
+
+app.post("/adduserwithlocation", function(req, res) {
+    console.log(req.body);
+    users.push({username: req.body.username, latitude: req.body.latitude, longitude: req.body.longitude});
+    console.log(users);
+    res.send(users);
+})
+
+app.put("/updateuserwithlocation", function(req, res) {
+    for (let i = 0; index < users.length; i++) {
+        if(users[i].username == req.body.username) {
+            users[i].latitude = req.body.latitude;
+            users[i].longitude = req.body.longitude;
+        }
+    }
+    console.log(users);
+    res.send(users);
+}
+)
